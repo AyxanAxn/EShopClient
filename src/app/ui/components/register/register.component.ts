584e5251063007a454fad762f237db8e350dea21
Aykhan <axundovayxan9@gmail.com>
2022-09-24 03:05:41 +0400

    Added reactiveForm

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -1,4 +1,6 @@
+import { trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) { }
 
+
+  frm : FormGroup;
   ngOnInit(): void {
+    this.frm=this.formBuilder.group({
+      nameSurname:["",
+      [Validators.required,
+        Validators.maxLength(50),
+        Validators.minLength(3)]],
+      username:["",
+      [Validators.required,
+        Validators.maxLength(50),
+        Validators.minLength(3)]
+    ],
+      email:["",
+      [Validators.required,
+        Validators.maxLength(50),
+        Validators.email]],
+      password:[""],
+      confirmEmail:[""]
+    });
   }
 
+  //  get is used to create a parametr:
+    get component(){
+      return this.frm.controls;
+    }
+  onSubmit(data: any){
+    debugger;
+  }
 }
